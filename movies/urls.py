771d04d0abd2b3575a689539831b4bce56d08295
risingcurve <gksdnjswhd10@naver.com>
2022-11-15 13:57:59 +0900

    accounts

--- movies/urls.py
+++ movies/urls.py
@@ -2,3 +2,6 @@ from django.urls import path
 from . import views
 
 app_name = 'articles'
+urlpatterns = [
+    path('', views.index, name='index'),
+]
--- mypjt/settings.py
+++ mypjt/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'movies',
     'accounts',
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
